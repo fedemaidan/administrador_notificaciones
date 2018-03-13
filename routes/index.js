11d0a33dd3a0d36a8f9c3b28738a7926d673e4f1
fede <federico.maidan@gaudi.com.ar>
2018-03-13 12:23:13 -0300

    dame token

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 var agregarCuentaController     = require('../controller/agregarCuenta.js'); 
 var escuchoNotificaciones 		= require('../controller/escuchoNotificaciones.js'); 
+var datosCuenta 		= require('../controller/datosCuenta.js'); 
 
 router.get('/iniciarConML', 	agregarCuentaController.iniciarMl);
 router.get('/auth_ml', 			agregarCuentaController.authMl);
 router.post('/escucho', 		escuchoNotificaciones.escuchoNotificaciones );
+router.get('/token',			datosCuenta.dameToken );
 
 module.exports = router;
 
