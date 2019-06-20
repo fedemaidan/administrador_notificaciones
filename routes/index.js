34e8a40538e3ac649b76d54da051ae7370d3215a
Tu Nombre <you@example.com>
2019-06-20 16:52:41 -0300

    get notificaciones

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 var agregarCuentaController     = require('../controller/agregarCuenta.js'); 
 var escuchoNotificaciones 		= require('../controller/escuchoNotificaciones.js'); 
 var datosCuenta 		= require('../controller/datosCuenta.js'); 
+var notificaciones 		= require('../controller/notificaciones.js'); 
 
 router.get('/iniciarConML', 	agregarCuentaController.iniciarMl);
 router.get('/auth_ml', 			agregarCuentaController.authMl);
+router.get('/notificaciones', notificaciones.get);
 router.post('/escucho', 		escuchoNotificaciones.escuchoNotificaciones );
 router.get('/token',			datosCuenta.dameToken );
 
 module.exports = router;
-
